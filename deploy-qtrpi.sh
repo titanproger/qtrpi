0454037526d2bee86f440fe3c652b8a09906982a
ProninE <titan.the.proger@gmail.com>
2018-04-17 18:55:33 +0300

    Add  modules qtlocation qtgraphicaleffects
    Modifications for debian stretch

--- deploy-qtrpi.sh
+++ deploy-qtrpi.sh
@@ -45,15 +45,24 @@ if [[ $PREPARE_RPI ]]; then
     TARGET_USER=$(echo $TARGET_HOST | cut -d@ -f1)
     ssh $TARGET_HOST "sudo mkdir /usr/local/qt5pi ; sudo chown -R ${TARGET_USER}:users /usr/local/qt5pi"
     ssh $TARGET_HOST 'sudo apt-get install -y apt-transport-https'
-    ssh $TARGET_HOST 'sudo apt-get install -y libts-0.0-0 libinput5 fontconfig'
+    ssh $TARGET_HOST 'sudo apt-get install -y libts-0.0-0 libinput10 fontconfig mc'
     ssh $TARGET_HOST "sudo sh -c 'echo /usr/local/qt5pi/lib > /etc/ld.so.conf.d/99-qt5pi.conf'"
+    # TODO change /etc/gai.conf to force ipv4
 
+ssh $TARGET_HOST 'sudo apt-get install -y libharfbuzz0b libpcre16-3 libdouble-conversion1 libxkbcommon0'
     # to fix which version of libEGL should be picked by Qt applications (/opt/vc rather than /usr/lib/....)
+    # NOTE for debian stretch: see @link https://www.raspberrypi.org/blog/raspbian-stretch/#comment-1321445
+    #          libharfbuzz0b  +
+    #          libpcre16-3  +
+    #          libdouble-conversion1 +
+    #          libxkbcommon0 +
     ssh $TARGET_HOST "sudo sh -c 'rm /usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0 /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0'"
-    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0'"
-    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1'"
-    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0'"
-    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2'"
+    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libbrcmEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0'"
+    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libbrcmEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so.1'"
+    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libbrcmEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so'"
+    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libbrcmGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0'"
+    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libbrcmGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so.2'"
+    ssh $TARGET_HOST "sudo sh -c 'ln -sf /opt/vc/lib/libbrcmGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so'"
 fi
 
 rsync -avz $OUTPUT_DIR $TARGET_HOST:/usr/local/
