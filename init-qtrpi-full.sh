e9bbbf865c8d415fdca3616782d7cdb7228a96d7
ProninE <titan.the.proger@gmail.com>
2018-05-19 01:38:21 +0300

    Multimedia module 5.7.1

--- init-qtrpi-full.sh
+++ init-qtrpi-full.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #export QTRPI_QT_VERSION='5.10.1'
-export QTRPI_QT_VERSION='5.8.0'
+export QTRPI_QT_VERSION='5.7.1'
 export QTRPI_TARGET_DEVICE='linux-rpi3-g++'
 #export QTRPI_TARGET_DEVICE='linux-rasp-pi3-g++'
 export QTRPI_TARGET_HOST='pi@raspberry'
-export NO_DOWNLOAD=true
+export NO_QUESTIONS=true
 
 source ${0%/*}/utils/common.sh
 
@@ -47,14 +47,11 @@ check_env_vars
 
 cd utils
 ./init-common.sh
-
-#./synchronize-toolchain.sh
-#./download-raspbian.sh $RASPBIAN_ARG
-#./prepare-sysroot-full.sh
-
+./synchronize-toolchain.sh
+./download-raspbian.sh $RASPBIAN_ARG
+./prepare-sysroot-full.sh
 # you shouln not use minimal
 #./prepare-sysroot-minimal.sh
-
 ./switch-sysroot.sh full
 ./synchronize-qt-modules.sh --clean-all
 ./compile-qt-modules.sh
